988e0a0f71cfd1778464d72f64967acf15bace99
mksiddiq <mksiddiq99100@gmail.com>
2022-10-19 16:39:11 +0530

    third commit bugs resolved

--- app/src/main/java/com/siddiq/woopie/activity/ForgotPasswordActivity.kt
+++ app/src/main/java/com/siddiq/woopie/activity/ForgotPasswordActivity.kt
@@ -39,79 +39,91 @@ class ForgotPasswordActivity : AppCompatActivity() {
         val url = "http://13.235.250.119/v2/forgot_password/fetch_result"
 
         btnNext.setOnClickListener {
-            btnNext.visibility = View.GONE
-            progressBarFP.visibility = View.VISIBLE
+            if (etMobileNumberFP.text.isEmpty() || etEmailFP.text.isEmpty()) {
+                Toast.makeText(this@ForgotPasswordActivity,
+                    "Please enter all the credentials first",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                //btnNext.visibility = View.GONE
+                progressBarFP.visibility = View.VISIBLE
 
-            val mobileNumber = etMobileNumberFP.text.toString()
-            val email = etEmailFP.text.toString()
+                val mobileNumber = etMobileNumberFP.text.toString()
+                val email = etEmailFP.text.toString()
 
-            val jsonParams = JSONObject()
-            jsonParams.put("mobile_number", mobileNumber)
-            jsonParams.put("email", email)
+                val jsonParams = JSONObject()
+                jsonParams.put("mobile_number", mobileNumber)
+                jsonParams.put("email", email)
 
-            if(ConnectionManager().checkConnectivity(this@ForgotPasswordActivity)){
-                val jsonObjectRequest =
-                    object : JsonObjectRequest(Request.Method.POST, url, jsonParams, Response.Listener {
-                        try {
-                            progressBarFP.visibility = View.GONE
-                            val data = it.getJSONObject("data")
-                            val success = data.getBoolean("success")
-                            val firstTry= data.getBoolean("first_try")
-                            if(success){
-                                if(firstTry){
-                                    val intent = Intent(this@ForgotPasswordActivity, OTPActivity::class.java)
+                if (ConnectionManager().checkConnectivity(this@ForgotPasswordActivity)) {
+                    val jsonObjectRequest =
+                        object : JsonObjectRequest(Request.Method.POST,
+                            url,
+                            jsonParams,
+                            Response.Listener {
+                                try {
+                                    progressBarFP.visibility = View.GONE
+                                    val data = it.getJSONObject("data")
+                                    val success = data.getBoolean("success")
+                                    val firstTry = data.getBoolean("first_try")
+                                    if (success) {
+                                        if (firstTry) {
+                                            val intent = Intent(this@ForgotPasswordActivity,
+                                                OTPActivity::class.java)
+                                            intent.putExtra("mobileNumber", mobileNumber)
+                                            Toast.makeText(this@ForgotPasswordActivity,
+                                                "OTP sent to registered email",
+                                                Toast.LENGTH_SHORT).show()
+                                            startActivity(intent)
+                                        } else {
+                                            val intent = Intent(this@ForgotPasswordActivity,
+                                                OTPActivity::class.java)
+                                            intent.putExtra("mobileNumber", mobileNumber)
+                                            Toast.makeText(this@ForgotPasswordActivity,
+                                                "Please use the previously sent OTP to reset password",
+                                                Toast.LENGTH_SHORT).show()
+                                            startActivity(intent)
+                                        }
+                                    } else {
+                                        Toast.makeText(this@ForgotPasswordActivity,
+                                            "User not found",
+                                            Toast.LENGTH_SHORT).show()
+                                    }
+                                } catch (e: JSONException) {
                                     Toast.makeText(this@ForgotPasswordActivity,
-                                        "OTP sent to registered email",
+                                        "Some unexpected error occurred",
                                         Toast.LENGTH_SHORT).show()
-                                    startActivity(intent)
-                                } else{
-                                    val intent = Intent(this@ForgotPasswordActivity, OTPActivity::class.java)
-                                    Toast.makeText(this@ForgotPasswordActivity,
-                                        "Please use the previously sent OTP to reset password",
-                                        Toast.LENGTH_SHORT).show()
-                                    startActivity(intent)
                                 }
-                            } else{
+
+                            },
+                            Response.ErrorListener {
                                 Toast.makeText(this@ForgotPasswordActivity,
-                                    "User not found",
+                                    "Some error occurred!",
                                     Toast.LENGTH_SHORT).show()
+                            }) {
+                            override fun getHeaders(): MutableMap<String, String> {
+                                val headers = HashMap<String, String>()
+                                headers["Content-type"] = "application/json"
+                                headers["token"] = "5ed071d6b72b56"
+                                return headers
                             }
                         }
-                        catch (e: JSONException){
-                            Toast.makeText(this@ForgotPasswordActivity,
-                                "Some unexpected error occurred",
-                                Toast.LENGTH_SHORT).show()
-                        }
-
-                    }, Response.ErrorListener {
-                        Toast.makeText(this@ForgotPasswordActivity,
-                            "Some error occurred!",
-                            Toast.LENGTH_SHORT).show()
-                    }) {
-                        override fun getHeaders(): MutableMap<String, String> {
-                            val headers = HashMap<String, String>()
-                            headers["Content-type"] = "application/json"
-                            headers["token"] = "5ed071d6b72b56"
-                            return headers
-                        }
+                    queue.add(jsonObjectRequest)
+                } else {
+                    val dialog = AlertDialog.Builder(this@ForgotPasswordActivity)
+                    dialog.setTitle("Error")
+                    dialog.setMessage("Internet Connection Not Found")
+                    dialog.setPositiveButton("Open Settings") { text, listener ->
+                        val settingsIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                        startActivity(settingsIntent)
+                        finish()
                     }
-                queue.add(jsonObjectRequest)
-            } else{
-                val dialog = AlertDialog.Builder(this@ForgotPasswordActivity)
-                dialog.setTitle("Error")
-                dialog.setMessage("Internet Connection Not Found")
-                dialog.setPositiveButton("Open Settings") { text, listener ->
-                    val settingsIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
-                    startActivity(settingsIntent)
-                    finish()
+                    dialog.setNegativeButton("Exit App") { text, listener ->
+                        ActivityCompat.finishAffinity(this@ForgotPasswordActivity)
+                    }
+                    dialog.create()
+                    dialog.show()
                 }
-                dialog.setNegativeButton("Exit App") { text, listener ->
-                    ActivityCompat.finishAffinity(this@ForgotPasswordActivity)
-                }
-                dialog.create()
-                dialog.show()
             }
-
 
         }
     }
--- app/src/main/java/com/siddiq/woopie/activity/OTPActivity.kt
+++ app/src/main/java/com/siddiq/woopie/activity/OTPActivity.kt
@@ -7,6 +7,7 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -29,11 +30,16 @@ class OTPActivity : AppCompatActivity() {
     lateinit var btnSubmit: Button
     lateinit var progressBarOTP: ProgressBar
     lateinit var sharedPreferences: SharedPreferences
+    var mobileNumberIntent: String? = "100"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
 
         supportActionBar?.title = "Reset Password"
+
+        if(intent!=null){
+            mobileNumberIntent = intent.getStringExtra("mobileNumber") as String
+        }
 
         etOTP = findViewById(R.id.etOTP)
         etNewPassword = findViewById(R.id.etNewPassword)
@@ -48,71 +54,80 @@ class OTPActivity : AppCompatActivity() {
         val url = "http://13.235.250.119/v2/reset_password/fetch_result"
 
         btnSubmit.setOnClickListener {
-            progressBarOTP.visibility = View.VISIBLE
-            val otp = etOTP.text.toString()
-            val password = etNewPassword.text.toString()
-            val mobileNumber = ForgotPasswordActivity().etMobileNumberFP.text.toString()
+            if(etOTP.text.isEmpty()||etConfirmPasswordOTP.text.isEmpty()||etNewPassword.text.isEmpty()){
+                Toast.makeText(this@OTPActivity,
+                    "Please enter all the credentials first",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            } else{
+                progressBarOTP.visibility = View.VISIBLE
+                val otp = etOTP.text.toString()
+                val password = etNewPassword.text.toString()
+                val mobileNumber = mobileNumberIntent
 
-            val jsonParams = JSONObject()
-            jsonParams.put("mobile_number", mobileNumber)
-            jsonParams.put("password", password)
-            jsonParams.put("otp", otp)
+                val jsonParams = JSONObject()
+                jsonParams.put("mobile_number", mobileNumber)
+                jsonParams.put("password", password)
+                jsonParams.put("otp", otp)
 
-            if (ConnectionManager().checkConnectivity(this@OTPActivity)) {
-                val jsonObjectRequest = object :
-                    JsonObjectRequest(Request.Method.POST, url, jsonParams, Response.Listener {
-                        try {
-                            progressBarOTP.visibility = View.GONE
-                            val data = it.getJSONObject("data")
-                            val success = data.getBoolean("success")
-                            if(success){
-                                val intent =Intent(this@OTPActivity, LoginActivity::class.java)
-                                sharedPreferences.edit().clear().apply()
-                                startActivity(intent)
-                                finish()
+                if (ConnectionManager().checkConnectivity(this@OTPActivity)) {
+                    val jsonObjectRequest = object :
+                        JsonObjectRequest(Request.Method.POST, url, jsonParams, Response.Listener {
+                            try {
+                                progressBarOTP.visibility = View.GONE
+                                val data = it.getJSONObject("data")
+                                val success = data.getBoolean("success")
+                                if(success){
+                                    val intent =Intent(this@OTPActivity, LoginActivity::class.java)
+                                    sharedPreferences.edit().clear().apply()
+                                    startActivity(intent)
+                                    finish()
+                                    Toast.makeText(this@OTPActivity,
+                                        "Password has successfully changed.",
+                                        Toast.LENGTH_SHORT)
+                                        .show()
+                                } else{
+                                    Toast.makeText(this@OTPActivity,
+                                        "Error, password reset unsuccessful. Please enter correct otp ($it)",
+                                        Toast.LENGTH_SHORT)
+                                        .show()
+                                }
+                            } catch (e: JSONException) {
                                 Toast.makeText(this@OTPActivity,
-                                    "Password has successfully changed",
+                                    "Some unexpected error occurred!",
                                     Toast.LENGTH_SHORT)
                                     .show()
-                            } else{
-                                Toast.makeText(this@OTPActivity,
-                                    "Error, password reset unsuccessful. Please enter correct otp",
-                                    Toast.LENGTH_SHORT)
-                                    .show()
+                                Log.e("CREATION", e.toString())
                             }
-                        } catch (e: JSONException) {
-                            Toast.makeText(this@OTPActivity,
-                                "Some unexpected error occurred!",
-                                Toast.LENGTH_SHORT)
+                        }, Response.ErrorListener {
+                            Toast.makeText(this@OTPActivity, "Some error occurred!", Toast.LENGTH_SHORT)
                                 .show()
+                        }) {
+                        override fun getHeaders(): MutableMap<String, String> {
+                            val headers = HashMap<String, String>()
+                            headers["Content-type"] = "application/json"
+                            headers["token"] = "5ed071d6b72b56"
+                            return headers
                         }
-                    }, Response.ErrorListener {
-                        Toast.makeText(this@OTPActivity, "Some error occurred!", Toast.LENGTH_SHORT)
-                            .show()
-                    }) {
-                    override fun getHeaders(): MutableMap<String, String> {
-                        val headers = HashMap<String, String>()
-                        headers["Content-type"] = "application/json"
-                        headers["token"] = "5ed071d6b72b56"
-                        return headers
                     }
+                    queue.add(jsonObjectRequest)
+                } else {
+                    val dialog = AlertDialog.Builder(this@OTPActivity)
+                    dialog.setTitle("Error")
+                    dialog.setMessage("Internet Connection Not Found")
+                    dialog.setPositiveButton("Open Settings") { text, listener ->
+                        val settingsIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                        startActivity(settingsIntent)
+                        finish()
+                    }
+                    dialog.setNegativeButton("Exit App") { text, listener ->
+                        ActivityCompat.finishAffinity(this@OTPActivity)
+                    }
+                    dialog.create()
+                    dialog.show()
                 }
-                queue.add(jsonObjectRequest)
-            } else {
-                val dialog = AlertDialog.Builder(this@OTPActivity)
-                dialog.setTitle("Error")
-                dialog.setMessage("Internet Connection Not Found")
-                dialog.setPositiveButton("Open Settings") { text, listener ->
-                    val settingsIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
-                    startActivity(settingsIntent)
-                    finish()
-                }
-                dialog.setNegativeButton("Exit App") { text, listener ->
-                    ActivityCompat.finishAffinity(this@OTPActivity)
-                }
-                dialog.create()
-                dialog.show()
             }
+
         }
     }
 }
